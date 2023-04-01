d91a4217a778073bc49c35ac5ba5071791e3db1f
Mac Demarco <vandanstars@yahoo.com>
2023-04-01 22:35:15 +0700

    feat: update routes

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+const {authenticateUser} = require('./middleware/authorization');
 
 
 // Routes
 const merchantRouter = require('./routes/merchants');
 const productRouter = require('./routes/products');
 
+// Merchant Router
 app.get('/merchants', merchantRouter.getMerchant);
 app.post('/merchants', merchantRouter.createMerchant);
+app.delete('/merchants/:id', merchantRouter.deleteMerchant);
+
+// Product Router
+app.get('/products', authenticateUser, productRouter.getMerchantProducts);
+app.post('/products', authenticateUser, productRouter.addProduct);
+app.delete('/products/:id', authenticateUser, productRouter. deleteProduct);
+app.put('/products/:id', authenticateUser, productRouter.updateProduct);
 
 // Server Port
 app.listen(3000, () => {
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,65 @@
-const express = require('express');
-const router = express.Router();
+const db = require('../config/database');
+const auth = require('basic-auth');
 
-router.get('/products', (req, res) => {
-  res.send('hello');
-});
 
-module.exports = router;
+// Add new product
+const addProduct = async (req,res) => {
+  const user = auth(req);
+  try {
+    await db.none('INSERT INTO products (name, quantity, price, merchant_id) VALUES($1, $2, $3, $4)', 
+    [req.body.name, req.body.quantity, req.body.price, user.name]);
+    res.status(201).send('New product has successfully added');
+  } catch (err) {
+    res.send(err.message);
+  }
+}
+
+const deleteProduct = async (req, res) => {
+  const user = auth(req);
+  const productExist = await db.manyOrNone('SELECT id FROM products WHERE merchant_id = $1 AND id = $2', 
+  [user.name, req.params.id]);
+  try {
+    if(!productExist) {
+      res.status(404).send(`Sorry, the product is not found on this merchant`);
+    }
+
+    // Delete the product if product exist
+    db.none(`DELETE FROM products WHERE merchant_id = $1 AND id = $2`, [user.name, req.params.id]);
+    res.send(`Product has successfully deleted`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+}
+
+const updateProduct = async (req, res) => {
+  const user = auth(req);
+  const productExist = await db.oneOrNone('SELECT name FROM products WHERE merchant_id = $1 AND id = $2', 
+  [user.name, req.params.id]);
+  try {
+    if(!productExist) {
+      res.send(404).send(`Can't update product, the product is not found on this merchant`);
+    }
+
+    await db.none(`
+    UPDATE products SET
+      name = $1,
+      quantity = $2,
+      price = $3
+    WHERE merchant_id = $4 AND id = $5`, [req.body.name, req.body.quantity, req.body.price, user.name, req.params.id]);
+    res.send(`Product ${req.body.name} has successfully updated`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+}
+
+const getMerchantProducts = async (req, res) => {
+  const user = auth(req);
+  try {
+    const products = await db.manyOrNone('SELECT * FROM products WHERE merchant_id = $1', [user.name]);
+    res.send(products);
+  } catch (error) {
+    res.send(error.message);
+  }
+}
+
+module.exports = {addProduct, deleteProduct, updateProduct, getMerchantProducts};
