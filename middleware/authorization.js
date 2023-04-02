268f44fb3546e70ddf8bb6128226be5404c59465
Mac Demarco <vandanstars@yahoo.com>
2023-04-02 14:20:16 +0700

    fixing the middleware when user not found

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -5,9 +5,12 @@ const bcrypt = require('bcrypt');
 const authenticateUser = async (req, res, next) => {
   const user = auth(req);
   const dbAuthentication = await db.oneOrNone('SELECT id, password FROM merchants WHERE id = $1', [user.name]);
-  const comparePass = bcrypt.compareSync(user.pass, dbAuthentication.password);
+  if (!dbAuthentication) {
+    res.status(404).send('User not found');
+    return;
+  };
   
-
+  const comparePass = bcrypt.compareSync(user.pass, dbAuthentication.password);
   if (comparePass) next();
   else res.status(401).send('Unauthorized User');
 }
